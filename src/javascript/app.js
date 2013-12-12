bd57df1e0c1b497d9f1d48d2199522710256d9d4
John Martin <curmudgeon@rallydev.com>
2013-12-12 13:00:56 -0800

    add cost selector

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,10 +4,101 @@ Ext.define('CustomApp', {
 
     logger: new Rally.technicalservices.Logger(),
     items: [
-        {xtype:'container',itemId:'message_box',tpl:'Hello, <tpl>{_refObjectName}</tpl>'},
+        {xtype:'container',itemId:'settings_box', defaults: {padding: 10, margin: 10 }},
+        {xtype:'container',itemId:'buttons_box', defaults: {padding:10,margin:10}},
         {xtype:'tsinfolink'}
     ],
     launch: function() {
-        this.down('#message_box').update(this.getContext().getUser());
+        this.project = this.getContext().getProject();
+        this.logger.log("Project:", this.project.Name);
+        //TODO: make sure this user has permission
+        //TODO: check settings
+        this._settingFields = [
+            { dataIndex:'Cost', type: 'rallynumberfield' },
+            { dataIndex:'PIL', type: 'pil_combo' },
+            { dataIndex:'Stage', type: 'stage_combo' },
+            { dataIndex:'foo', type: 'rallytextfield' }
+        ];
+        this._getCurrentSettings();
+        
+//        this.settings = {
+//            foo: "John was here",
+//            bar: "So were Eloi and Adrie"
+//        };
+//        
+//        this._saveSettings();
+    },
+    _getCurrentSettings: function() {
+        Rally.data.PreferenceManager.load({
+            project: this.getContext().getProject(),
+            filterByName: 'philips.project.settings',
+            scope: this,
+            success: function(prefs) {
+                if ( prefs && prefs['philips.project.settings'] ) {
+                    this.settings = Ext.JSON.decode(prefs['philips.project.settings']);
+                }
+                this.logger.log("pref retrieved: ", this.settings );
+                this._displaySettings();
+                this._addButtons();
+            }
+        });
+    },
+    _addButtons: function() {
+        this.down('#buttons_box').add({
+            itemId:'save_button',
+            xtype:'rallybutton',
+            text:'Save Settings',
+            listeners: {
+                scope: this,
+                click: function(button) {
+                    button.setDisabled(true);
+                    this._saveSettings();
+                }
+            }
+        });
+    },
+    _displaySettings: function() {
+        var me = this;
+        // Put up greeting notice
+        this.down('#settings_box').add({
+            xtype:'container',
+            html: 'To set "special" properties for the project called ' +
+                this.getContext().getProject().Name + 
+                ', change the values below'
+        });
+        // cycle through the fields and display them for changing
+        Ext.Array.each(this._settingFields, function(field) {
+            var type = field.type;
+            var label = field.dataIndex;
+            var value = me.settings[label];
+            if ( type == 'rallynumberfield' ) {
+                me.down('#settings_box').add({
+                    xtype: type,
+                    value: value,
+                    fieldLabel: label,
+                    listeners: {
+                        change: function( numberfield, new_value ) {
+                            me.settings[label] = new_value;
+                        }
+                    }
+                });
+            }
+        });
+    },
+    _saveSettings: function() {
+        var me = this;
+        // save whatever all the current settings are
+        Rally.data.PreferenceManager.update({
+            project: this.getContext().getProject(),
+            settings: {
+                'philips.project.settings': Ext.JSON.encode(this.settings)
+            },
+            success: function(updatedRecords, notUpdatedRecords) {
+                me.down('#save_button').setDisabled(false);
+            },
+            failure: function() {
+                alert('there was a problem');
+            }
+        });
     }
 });
